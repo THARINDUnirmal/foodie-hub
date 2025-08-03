542486eac7fbf86f85efb28d0ba28a79a782b767
THARINDUnirmal <tharinduna0@gmail.com>
2025-08-03 19:02:00 +0530

    add some featuers

---
+++ script.js
@@ -0,0 +1,7 @@
+function openDialer(phoneNumber) {
+  window.location.href = "tel:" + phoneNumber;
+}// JavaScript Document
+
+function openEmailApp() {
+  window.location.href = "mailto:tharindunirmal1111@gmail.com?subject=Hello&body=Type your message here";
+}

f9db20b5bc40e226252729cc40ab66b73f0cc33c
THARINDUnirmal <tharinduna0@gmail.com>
2025-08-03 22:08:53 +0530

    update files

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ function openDialer(phoneNumber) {
 function openEmailApp() {
   window.location.href = "mailto:tharindunirmal1111@gmail.com?subject=Hello&body=Type your message here";
 }
+
+function openWhatsApp(number) {
+  const url = "https://wa.me/" + number;
+  window.open(url, '_blank');
+}
